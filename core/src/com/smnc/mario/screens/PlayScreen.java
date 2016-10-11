42749a2eedc05c81505c6dc0c68840678787ce66
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2016-10-12 01:21:24 +0300

    Render tile maps.

--- core/src/com/smnc/mario/screens/PlayScreen.java
+++ core/src/com/smnc/mario/screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -24,11 +27,24 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen(Mario game) {
         this.game = game;
+        // create cam used to follow mario through cam world
         gameCam = new OrthographicCamera();
+        // create a FitViewport to maintain visual aspect ration
         gamePort = new FitViewport(Mario.V_WIDTH, Mario.V_HEIGHT, gameCam);
+        // create out game HUD for scores/times/level info.
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
     }
 
     @Override
@@ -36,11 +52,28 @@ public class PlayScreen implements Screen {
 
     }
 
+    public void handleInput(float dt) {
+        if(Gdx.input.isTouched())
+            gameCam.position.x += 100 * dt;
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
+        // clear game screen with black
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        renderer.render();
+
+        // set out batch to now draw what the HUD camera sees.
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
     }
