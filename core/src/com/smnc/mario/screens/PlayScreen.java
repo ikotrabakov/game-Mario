fb3ad52ee16e16008e1b166d8afcac5ff93f567c
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2016-10-11 20:09:40 +0300

    Aspect ratios and viewports.

--- core/src/com/smnc/mario/screens/PlayScreen.java
+++ core/src/com/smnc/mario/screens/PlayScreen.java
@@ -3,8 +3,13 @@ package com.smnc.mario.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.smnc.mario.Mario;
+import com.sun.prism.image.ViewPort;
 
 /**
  * Created by smn on 10/11/16.
@@ -14,10 +19,14 @@ public class PlayScreen implements Screen {
 
     private Mario game;
     private Texture texture;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
 
     public PlayScreen(Mario game) {
         this.game = game;
         texture = new Texture("badlogic.jpg");
+        gameCam = new OrthographicCamera();
+        gamePort = new ScreenViewport(gameCam); // Good for the moment
     }
 
     @Override
@@ -29,6 +38,7 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(gameCam.combined);
         game.batch.begin();
         game.batch.draw(texture, 0, 0);
         game.batch.end();
@@ -36,7 +46,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width, height);
     }
 
     @Override
