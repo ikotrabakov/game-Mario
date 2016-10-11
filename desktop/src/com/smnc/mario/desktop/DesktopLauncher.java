799bf06e3e1388b309fca2759703880917a9f0ba
Ivaylo Kotrabakov <ikotrabakov@gmail.com>
2016-10-11 19:45:56 +0300

    Initial commit

---
+++ desktop/src/com/smnc/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.smnc.mario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.smnc.mario.Mario;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Mario(), config);
+	}
+}
